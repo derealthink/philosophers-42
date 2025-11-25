167cda78d34111ae80862325e6d182f39e19ef03
umberto <u.ponci@gmail.com>
2025-11-25 09:54:04 +0100

    starting threads

--- thread.c
+++ thread.c
@@ -1,16 +1,2 @@
 #include "philo.h"
 
-void    *test()
-{
-    printf("thread 1, works");
-    return NULL;
-}
-
-int main(void)
-{
-    pthread_t  thread_one = 0 ;
-
-    pthread_create(&thread_one, NULL, test, NULL);
-
-    pthread_join(thread_one, NULL);
-}
