5fa7d7459eab4d123ea9205e645f61644c29b00f
umberto <u.ponci@gmail.com>
2025-11-25 10:54:56 +0100

    last save home

--- thread.c
+++ thread.c
@@ -1,2 +1,14 @@
 #include "philo.h"
 
+int is_dead(t_philo *philo)
+{
+    pthread_mutex_lock(philo->dead_lock);
+    if (philo->dead == 1)
+    {
+        pthread_mutex_unlock(philo->dead_lock);
+        return (1);
+    }
+    pthread_mutex_unlock(philo->dead_lock);
+    return (0);
+}
+
