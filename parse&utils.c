ec6c8a550dce2e58ad4d922259e8b5eed08534c7
Umberto Ponci <uponci@c4a8c5.42berlin.de>
2025-11-25 12:29:54 +0100

    added time

--- parse&utils.c
+++ parse&utils.c
@@ -31,3 +31,20 @@ int is_dig_arr(char **s)
 	}
 	return (1);
 }
+
+size_t	get_time(void)
+{
+	struct	timeval time;
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+int	ft_usleep(size_t ms)
+{
+	size_t	start;
+
+	start = get_time();
+	while((get_time() - start) < ms)
+		usleep(500);
+	return (0);
+}
--- philo.h
+++ philo.h
@@ -6,6 +6,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <pthread.h>
+# include <sys/time.h>
 
 typedef  struct s_philo
 {
